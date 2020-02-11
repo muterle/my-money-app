d84b32a85bab1cf44342fbe12d9b2b7820fa3bbe
betto <betto@NOTE_BETTO>
2020-02-10 22:21:08 -0300

    Melhorando tratamento de erros

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //module.exports = mongoose.connect('mongodb://localhost/mymoney');
-module.exports = mongoose.connect('mongodb://teste:teste@cluster0-shard-00-00-k649d.mongodb.net:27017,cluster0-shard-00-01-k649d.mongodb.net:27017,cluster0-shard-00-02-k649d.mongodb.net:27017/mymoney?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+module.exports = mongoose.connect('mongodb://admin:admindb@cluster0-shard-00-00-k649d.mongodb.net:27017,cluster0-shard-00-01-k649d.mongodb.net:27017,cluster0-shard-00-02-k649d.mongodb.net:27017/mymoney?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useMongoClient: true 
 });
 
